58f8d2be1348ed45d1a20363f8f6a68a402f4b1d
icarofreire40-create <icarofreire40@gmail.com>
2026-06-03 08:22:13 -0300

    animação de entrada nas seções

--- script.js
+++ script.js
@@ -213,3 +213,19 @@ function animate() {
 
 initParticles();
 animate();
+
+/*======================
+ANIMAÇÃO DE ENTRADA
+======================*/
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        }
+    });
+}, { threshold: 0.1 });
+
+document.querySelectorAll('.section').forEach(section => {
+    observer.observe(section);
+});
